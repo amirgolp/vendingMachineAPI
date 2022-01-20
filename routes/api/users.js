d1e2d433002dac3181646a199a04a3a63c3af443
amirgolp <44730575+amirgolp@users.noreply.github.com>
2022-01-20 11:38:14 +0100

    a

--- routes/api/users.js
+++ routes/api/users.js
@@ -18,16 +18,7 @@ router.post('/', [
     //   .isEmail(),
     check('password', 'Please enter a password with 6 or more characters')
       .isLength({ min: 6 }),
-    check('deposit', 'Use a coin from 5, 10, 20, 20, 50, 100 cents only!')
-      .custom( val => {
-        if(val < 0) return false;
-        if(val < 10 && val%5 !== 0) return false;
-        if(val < 20 && val%10 !== 0) return false;
-        if(val < 50 && val%20 !== 0) return false;
-        if(val < 100 && val%50 !== 0) return false;
-        if(val < 101 && val%100 !== 0) return false;
-        return true;
-    }),
+
 ], async (req, res) => {
 
     const errors = validationResult(req);
@@ -37,6 +28,13 @@ router.post('/', [
     }
 
     const { username, password, deposit, email, role} = req.body;
+    
+    if (deposit!==undefined) {
+        coinType = deposit == 5 || deposit == 10 || deposit == 20 || deposit == 50 || deposit == 100
+        if (!coinType) {
+            return res.status(400).json({ errors: 'Use a coin from 5, 10, 20, 50, 100 cents only!'});
+        };
+    }
 
     try {
         let user = await User.findOne({ username });
@@ -77,7 +75,7 @@ router.post('/', [
 });
 
 // @route    DELETE /users
-// @desc     Delete user
+// @desc     Delete user id
 // @access   Private
 router.delete('/', auth, async (req, res) => {
     try {
@@ -87,6 +85,21 @@ router.delete('/', auth, async (req, res) => {
     } catch (err) {
         res.status(500).json({ msg: err })
     }
+});
+
+// @route    DELETE /users
+// @desc     Delete by username
+// @access   Private
+router.delete('/:username', auth, async (req, res) => {
+    const username = req.params.username;
+    console.log(username)
+    // try {
+    //     // Remove user
+    //     await User.findOneAndRemove({ _id: req.user.id });
+    //     res.json({ msg: 'user removed'})
+    // } catch (err) {
+    //     res.status(500).json({ msg: err })
+    // }
 });
 
 module.exports = router;
